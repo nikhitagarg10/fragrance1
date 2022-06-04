528e1503186d14251483e1308a1b252243ce482f
nikhitagarg10 <70847019+nikhitagarg10@users.noreply.github.com>
2022-06-04 12:30:48 +0530

    Update settings.py

--- eshop/settings.py
+++ eshop/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h-u%s=!37$*e3dz96npwu0%7ofe*yanfq2o2e@!)9mvp=nem2!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['https://fragrance1.herokuapp.com/', '127.0.0.1', 'fragrance1.herokuapp.com']
 #ALLOWED_HOSTS = []
